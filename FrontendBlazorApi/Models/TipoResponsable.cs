d6bc79f102c9e378d54a2da008111658f0d2b68e
JUAN MANUEL USUGA GALEANO <juanusuga1113848@correo.itm.edu.co>
2025-10-02 18:04:16 -0500

    Aceptar cambios remotos y descartar locales

---
+++ FrontendBlazorApi/Models/TipoResponsable.cs
@@ -0,0 +1,10 @@
+namespace ApiProyectoFinalAYSW.Models
+{
+    public class TipoResponsable
+    {
+        public int Id { get; set; };
+        public string? Titulo { get; set; };
+        public string? Descripcion { get; set; };
+    }
+}
+
