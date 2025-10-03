a1356242a6fbcf7c905d562e07f571e6ccee621c
JHORMAN ZAPATA DOMINGUEZ <jhormanzapata244383@correo.itm.edu.co>
2025-10-02 21:45:37 -0500

    Creacion de paginas Usuarios, Entregables, variables estrategicas. Se agregan iconos al menu de navegacion y se agregan todas las paginas para ser implementadas mas adelante

---
+++ FrontendBlazorApi/Models/Usuario.cs
@@ -0,0 +1,34 @@
+using System.Text.Json.Serialization;
+
+namespace FrontendBlazorApi.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Contrasena { get; set; } = string.Empty;
+        public string RutaAvatar { get; set; } = string.Empty;
+        public bool Activo { get; set; }
+
+    }
+
+    public class usuarioTratar
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Contrasena { get; set; } = string.Empty;
+        public string RutaAvatar { get; set; } = string.Empty;
+        public bool Activo { get; set; }
+
+    }
+
+    // Clase genérica para mapear la respuesta de la API
+    public class RespuestaApiUsuario<T>
+    {
+        public T? Datos { get; set; }
+
+    }
+
+    
+}
